d1a5fe31cc50ba0204539efb9f47068a7a11059a
Flobolo <Flobolo@users.noreply.github.com>
2018-01-12 10:08:50 +0100

    Test worked

--- src/test/scala/de/htwg/se/djokajkaeppeler/model/GameSpec.scala
+++ src/test/scala/de/htwg/se/djokajkaeppeler/model/GameSpec.scala
@@ -17,10 +17,36 @@ class GameSpec extends WordSpec with Matchers {
       game.grid = game.grid.set(1,0, Cell(CellStatus.BLACK))
       game.grid = game.grid.set(2,1, Cell(CellStatus.BLACK))
       game.grid = game.grid.set(1,2, Cell(CellStatus.BLACK))
-      game.playerAtTurn = game.player._2
-      println(game)
+
+      game.grid = game.grid.set(1,1, Cell(CellStatus.WHITE))
+
       game.checkIfMoveIsValid(1,1,Cell(CellStatus.WHITE)) should be(false)
     }
+   "When the player try to set wothout freedoms but with a hit" in {
+     game.grid = game.grid.set(1,1, Cell(CellStatus.EMPTY))
+
+     game.grid = game.grid.set(0,0, Cell(CellStatus.BLACK))
+     game.grid = game.grid.set(0,1, Cell(CellStatus.BLACK))
+     game.grid = game.grid.set(0,2, Cell(CellStatus.BLACK))
+     game.grid = game.grid.set(1,0, Cell(CellStatus.BLACK))
+     game.grid = game.grid.set(2,1, Cell(CellStatus.BLACK))
+     game.grid = game.grid.set(1,2, Cell(CellStatus.BLACK))
+     game.grid = game.grid.set(2,0, Cell(CellStatus.BLACK))
+     game.grid = game.grid.set(2,2, Cell(CellStatus.BLACK))
+
+     game.grid = game.grid.set(0,3, Cell(CellStatus.WHITE))
+     game.grid = game.grid.set(1,3, Cell(CellStatus.WHITE))
+     game.grid = game.grid.set(2,3, Cell(CellStatus.WHITE))
+     game.grid = game.grid.set(3,0, Cell(CellStatus.WHITE))
+     game.grid = game.grid.set(3,1, Cell(CellStatus.WHITE))
+     game.grid = game.grid.set(3,2, Cell(CellStatus.WHITE))
+
+     game.grid = game.grid.set(1,1, Cell(CellStatus.WHITE))
+
+     println(game)
+
+     game.checkIfMoveIsValid(1,1,Cell(CellStatus.WHITE)) should be (true)
+   }
 
 
   }
