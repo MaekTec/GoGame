301d4f60b852617ae7cabacd308264b9802a273e
Flobolo <floriandjokaj@gmail.com>
2018-01-21 16:48:09 +0100

    Gui on progess

---
+++ src/main/scala/de/htwg/se/djokajkaeppeler/aview/gui/Board.scala
@@ -0,0 +1,51 @@
+package de.htwg.se.djokajkaeppeler.aview.gui
+
+import java.awt.geom.{Ellipse2D, Line2D}
+
+import de.htwg.se.djokajkaeppeler.model.Grid
+import de.htwg.se.djokajkaeppeler.controller._
+import scala.swing._
+import scala.swing.{Component, Dimension}
+import java.awt.{BasicStroke, Color, Graphics2D, Toolkit}
+
+import de.htwg.se.go.aview.gui.SwingGui
+
+
+
+class Board(val controller: Controller, var componentSize: Dimension) extends Component{
+
+  componentSize.setSize((componentSize.height * 0.9) toInt, (componentSize.height* 0.9) toInt)
+  preferredSize = new Dimension(componentSize.width , componentSize.height)
+
+  override def paintComponent(g : Graphics2D) {
+    //println(self.getSize)
+    //println()
+
+    //this.preferredSize = new Dimension((windowSize.height * 0.7*0.9).toInt , (windowSize.height * 0.7*0.9).toInt)
+
+    listenTo(controller)
+    val board = controller.grid
+    val fields = board.size - 1
+
+
+
+    g.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING,
+      java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
+    g.setColor(new Color(220, 179, 92))
+
+    g.fillRect(0, 0, preferredSize.width ,preferredSize.height)
+    val squareSide = (preferredSize.height) - 50 min (preferredSize.width) - 50
+    val wid = squareSide / fields
+    val x0 = (preferredSize.width - squareSide)/2
+    val y0 = (preferredSize.height - squareSide)/2
+    g.setColor(Color.BLACK)
+    // vertical lines
+    for (x <- 0 to fields)
+      g.draw(new Line2D.Double(x0 + x * wid, y0, x0 + x * wid, y0 + squareSide))
+    // horizontal lines
+    for (y <- 0 to fields)
+      g.draw(new Line2D.Double(x0, y0 + y * wid, x0 + squareSide, y0 + y * wid))
+    g.setStroke(new BasicStroke(fields))
+
+  }
+}
--- src/main/scala/de/htwg/se/djokajkaeppeler/aview/gui/SwingGui.scala
+++ src/main/scala/de/htwg/se/djokajkaeppeler/aview/gui/SwingGui.scala
@@ -1,9 +1,18 @@
 package de.htwg.se.go.aview.gui
 
+import java.awt.Toolkit
+
+import de.htwg.se.djokajkaeppeler.aview.gui.Board
+
 import scala.swing._
 import scala.swing.Swing.LineBorder
 import scala.swing.event._
 import de.htwg.se.djokajkaeppeler.controller._
+import de.htwg.se.djokajkaeppeler.util.Observer
+
+import scala.swing.event.MouseClicked
+import scala.swing.event.MousePressed
+import scala.swing.event.MouseReleased
 import scala.io.Source._
 
 
@@ -11,31 +20,81 @@ class CellClicked(val row: Int, val column: Int) extends Event
 
 class SwingGui(controller: Controller) extends Frame {
 
+  def mouseClick(xCordinate: Int, yCordinate: Int, boardDimension: Dimension): Unit ={
+    val gridSize = controller.grid.size
+    println(xCordinate + " " + yCordinate + " dim:" + boardDimension)
+
+
+  }
+
+
+
+
+  var screeSize = Toolkit.getDefaultToolkit.getScreenSize
+  var windowSize = ((screeSize.width min screeSize.height) * 0.7).toInt
+  preferredSize = new Dimension(windowSize, windowSize)
   listenTo(controller)
 
-  title = "HTWG Go"
 
-  //contents = new Button("skip!")
+  menuBar = new MenuBar {
+
+    contents += new Menu("Edit") {
+      mnemonic = Key.E
+      contents += new MenuItem(Action("Undo") { controller.undo })
+      contents += new MenuItem(Action("Redo") { controller.redo })
+    }
+    contents += new Menu("Skip") {
+      mnemonic = Key.S
+      contents += new MenuItem(Action("Skip") { controller.skipTurn() })
+    }
+    contents += new Menu("Options") {
+      mnemonic = Key.O
+      //contents += new MenuItem(Action("Size 1*1") { controller.resize(1) })
+      //contents += new MenuItem(Action("Size 4*4") { controller.resize(4) })
+      //contents += new MenuItem(Action("Size 9*9") { controller.resize(9) })
+
+    }
+  }
 
 
-  val skipButton = new Button("skip!")
-  val panel = new FlowPanel{
+  val board = new Board(controller, preferredSize)
+
+
+  val panel = new FlowPanel(){
 
 
 
     //resizable = false
 
-    resizable = true
-    contents += skipButton
+
+
+      //add(skipButton, BorderPanel.Position.South)
+      //add(board, BorderPanel.Position.Center)
+
+
+    //contents += skipButton
+    contents += new BoxPanel(Orientation.Vertical) {
+      listenTo(this.mouse.clicks)
+      contents += board
+      reactions += {
+        case MousePressed(com, point, _, _, _) =>
+          println(this)
+          mouseClick(point.x, point.y, this.size)
+          board.repaint()
+      }
+    }
+
 
   }
 
-  listenTo(skipButton)
+
+
+
+
+
   reactions += {
-    case ButtonClicked(button) =>
-      if(button == skipButton) {
-        controller.skipTurn()
-      }
+    case event: GridSizeChanged =>
+    case event: Played     => repaint()
   }
 
 
@@ -46,5 +105,10 @@ class SwingGui(controller: Controller) extends Frame {
   }
 
 
+
+  title = "HTWG Go"
+  resizable = true
   visible = true
 }
+
+
